6999d3133d633f790f8cfe8da4cac53d502adeed
Denny Bartelt <d.bartelt@mairdumont-netletix.com>
2019-09-20 16:35:23 +0200

    fix type guard

--- src/utils.ts
+++ src/utils.ts
@@ -1,5 +1,5 @@
 export const isFunction = (x: any): x is Function => 'function' === typeof x;
 
-export const isBoolean = (x: any): x is Boolean => 'boolean' === typeof x;
+export const isBoolean = (x: any): x is boolean => 'boolean' === typeof x;
 
-export const isString = (x: any): x is String => 'string' === typeof x;
+export const isString = (x: any): x is string => 'string' === typeof x;
